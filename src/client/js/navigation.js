343286865aafa2108b198c9d9bba06e8474621fe
hikmanisyariful <syarifulfajar017@gmail.com>
2021-12-10 15:11:18 +0700

    Fix style navigation to form

--- src/client/js/navigation.js
+++ src/client/js/navigation.js
@@ -2,13 +2,16 @@ const navigation = route => {
   console.log(route);
   const form = document.getElementById("form-input");
   const results = document.getElementById("results");
+  const toForm = document.getElementById("to-form");
 
   if (route === "results") {
     form.style.display = "none";
     results.style.display = "flex";
+    toForm.style.display = "block";
   } else if (route === "form") {
     form.style.display = "flex";
     results.style.display = "none";
+    toForm.style.display = "none";
   }
 };
 
